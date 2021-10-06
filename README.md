71192ed7b354b9724252bca06a0de0fa913b59aa
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-10-06 02:02:58 -0300

    add real parameter store handler

---
+++ README.md
@@ -0,0 +1,24 @@
+#AWStanding
+Easily load variables from AWS Parameter store into environment variables.
+
+# Quickstart
+```python
+from awstanding.parameter_store import load_parameters
+load_parameters({'/some/path/to/something/stored': 'IMPORTANT_SETTING'})
+
+import os
+print(os.environ.get('IMPORTANT_SETTING'))
+'super-important-value'
+```
+
+# Using with python-decouple
+```python
+import os
+from awstanding.parameter_store import load_parameters
+from decouple import config
+load_parameters({'/some/path/to/something/stored': 'IMPORTANT_SETTING'})
+
+IMPORTANT_SETTING = config('IMPORTANT_SETTING', default='some-default')
+print(IMPORTANT_SETTING)
+'super-important-value'
+```
--- src/awstanding/parameter_store.py
+++ src/awstanding/parameter_store.py
@@ -1,2 +1,41 @@
-def load_parameters():
-    return 3
+"""Parameter Store Loader
+Use (setting_name, cast function) or setting_name as lookup value.
+If no cast function is passed, the parameter will be stored as retrieved
+from Parameter Store, typically string or stringList.
+
+Usage:
+from awstanding.parameter_store import load_parameters
+
+LOOKUP_DICT = {
+    '/my/parameter/path': 'NEW_VARIABLE'
+}
+
+load_parameters(LOOKUP_DICT)
+
+# Now NEW_VARIABLE can be obtained from environment variables.
+
+"""
+import os
+import boto3
+
+
+def load_parameters(lookup_dict):
+    ssm = boto3.client(service_name='ssm')
+    paginated_keys = (list(lookup_dict.keys())[i:i+10] for i in range(0, len(lookup_dict), 10))
+
+    parameters_ps = []
+    invalid_parameters = []
+    for keys in paginated_keys:
+        parameters_page = ssm.get_parameters(Names=keys, WithDecryption=True)
+        parameters_ps += parameters_page['Parameters']
+        invalid_parameters += parameters_page['InvalidParameters']
+
+    parameters_ps = {param['Name']: param['Value'] for param in parameters_ps}
+
+    # Override configuration for requested keys
+    for key in parameters_ps:
+        if isinstance(lookup_dict[key], (tuple, list)):
+            setting_name, cast = lookup_dict[key]
+            os.environ[setting_name] = cast(parameters_ps[key])
+        elif isinstance(lookup_dict[key], str):
+            os.environ[lookup_dict[key]] = parameters_ps[key]
